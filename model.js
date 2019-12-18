7349773df1d972370f7324c684c2a979d9b078b6
Anil Kumar <anil.k@lotusus.com>
2019-12-18 04:38:04 -0500

    fix for path

--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const templates = require('es6-template-strings');
 module.exports = (args) => {
-    const contents = fs.readFileSync(`${__dirname}/templates/model.txt`, 'utf8');
+    const contents = fs.readFileSync(`${process.env.PWD}/templates/model.txt`, 'utf8');
     if (args.name.substr(-1) === 'y') {
         args.name
     }
@@ -10,7 +10,7 @@ module.exports = (args) => {
         model: args.name.toLowerCase(),
         Models: args.name.substr(-1) === 'y' ? args.name.substr(0, args.name.length - 1) + 'ies' : args.name + 's',
     })
-    fs.writeFile(`${__dirname}/models/${args.name}.js`, parsed, function (err) {
+    fs.writeFile(`${process.env.PWD}/models/${args.name}.js`, parsed, function (err) {
         if (err) throw err;
         console.log(`Model ${args.name} is created successfully`);
     });
